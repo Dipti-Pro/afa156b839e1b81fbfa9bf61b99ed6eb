be5b2f32a44a694766d91fbba2024f912296800d
Dipti-Pro <85604091+Dipti-Pro@users.noreply.github.com>
2021-09-18 12:43:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -39,18 +39,3 @@ function draw() {
 }
 
 //Function to read values from DB
-function readStock(data){
-  foodS=data.val();
-}
-
-//Function to write values in DB
-function writeStock(x){
-  if(x<=0){
-    x=0;
-  }else{
-    x=x-1;
-  } 
-  database.ref('/').update({
-    Food:x
-  })
-}
